b41f464416f567ade2a8887fa3bdacb62b58e765
Israel Antezana Rojas <israelantezana@gmail.com>
2021-10-12 17:10:47 -0400

    Correcciones menores

--- src/CalculadoraCadenas.js
+++ src/CalculadoraCadenas.js
@@ -2,7 +2,7 @@ export function sumarCadena(cadena) {
   const numeros = cadena.split(",");
   let total = 0;
   numeros.forEach((numeroCadena) => {
-    total = total + Number(numeroCadena);
+    total += Number(numeroCadena);
   });
   return total;
 }
